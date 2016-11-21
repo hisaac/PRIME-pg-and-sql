9e1697b1c9d9c6c79d2d4baad8bb82028453b625
Isaac Halvorson <hello@hisaac.net>
2016-11-21 13:50:04 -0600

    build treats module and build database GET function

--- server/app.js
+++ server/app.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var port = process.env.PORT || 3000;
-var connectionString = "postgres://localhost:5432/sigma";
+var connectionString = "postgres://localhost:5432/sweets";
+var treats = require('./modules/treats.js');
+var bodyParser = require('body-parser');
 
 /*** Build out a module to manage our treats requests. ***/
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/treats', treats);
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('./server/public/views/index.html'));
---
+++ server/modules/treats.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+var config = {
+  database: 'sweets'
+};
+
+var pool = new pg.Pool(config);
+
+router.get('/', function (req, res) {
+  pool.connect()
+    .then(function (client) {
+      client.query("SELECT * FROM treats")
+        .then(function (result){
+          client.release();
+          res.send(result.rows);
+        }); //<----------------------------------.then
+    }) //<---------------------------------------.then
+    .catch(function (err){
+      console.log('error on SELECT', err);
+      res.sendStatus(500);
+    }); //<-------------------------------------.catch
+}); //<-------------------------------------router.get
+
+router.post('/', function (req, res) {
+  var newTreat = req.body;
+
+  pool.connect().then(function (client) {
+    client.query("")
+  });
+});
+
+module.exports = router;
