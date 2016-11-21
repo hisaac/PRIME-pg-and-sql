c4c6c9e9dff0deaa2122423a17ef72c90ad2ef3f
Isaac Halvorson <hello@hisaac.net>
2016-11-21 13:20:57 -0600

    setup database and save queries into create_database.sql file

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,15 @@
 -- Database name
+sweets
 
 -- Document your create tables SQL here
+CREATE TABLE treats(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  description TEXT,
+  pic VARCHAR(128)
+);
+
+
+INSERT INTO treats (name, description, pic)
+VALUES ('Cupcake', 'A delicious cupcake', '/assets/cupcake.jpg'),
+('Donuts', 'Mmmm donuts', '/assets/donuts.jpg');
